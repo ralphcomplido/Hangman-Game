104f4a80bc37cab5132ebf77fcd8611fd3e499be
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-08-27 16:08:40 -0700

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// Hangman Game Pseudocode
+
+// 1. Ask user to press any key to get started
+
+ document.onkeyup = function(event) {
+
+// 2. Assign variable to the user input
+
+var userGuess = event.key;
+// 3. Create array that has the words that the user will guess
+
+// 4. Create a randomizer for the array of words that the user will guess
+
+// 5. Create a code that will determine if the key pressed matched any letter in the word
+
+// 6. Create a code that will determine if the key pressed doesn't match the key pressed.
+
+// 7. Assign number of guesses before the game be over
+
+// 8. Create a code to show which letter are already guessed
+
+// 9. Create a code that will not deduct the number of guesses if the user input is repeated.
+
